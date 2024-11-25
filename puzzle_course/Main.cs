34be4d044ae4a68620cc2619305ead6011de5f63
William Koppelberger <koppelbw@gmail.com>
2024-11-25 15:37:07 -0500

    Section 1 Complete

--- puzzle_course/Main.cs
+++ puzzle_course/Main.cs
@@ -1,16 +1,63 @@
 using Godot;
 using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Game;
 
 public partial class Main : Node2D
 {
+	private Sprite2D cursor;
+	private PackedScene buildingScene;
+	private Button placeBuildingButton;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello world");
+		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
+		cursor = GetNode<Sprite2D>("Cursor");
+		placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
+
+		cursor.Visible = false;
+
+		placeBuildingButton.Pressed += OnButtonPressed;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    public override void _UnhandledInput(InputEvent evt)
+    {
+        if(cursor.Visible && evt.IsActionPressed("left_click"))
+		{
+			PlaceBuildingAtMousePosition();
+			cursor.Visible = false;
+		}
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{		
+		cursor.GlobalPosition = GetMouseGridCellPosition() * 64;
+	}
+
+
+
+	private Vector2 GetMouseGridCellPosition()
 	{
+		var mousePosition = GetGlobalMousePosition();
+		var gridPosition = (mousePosition / 64).Floor();
+
+		return gridPosition;
+	}
+
+	private void PlaceBuildingAtMousePosition()
+	{
+		var building = buildingScene.Instantiate<Node2D>();
+		AddChild(building);
+
+		var gridPosition = GetMouseGridCellPosition() * 64;
+		building.GlobalPosition = gridPosition;
+	}
+
+	private void OnButtonPressed()
+	{
+		cursor.Visible = true;
 	}
 }
